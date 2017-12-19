4e982dedfdf0eb0b900378a09d9700893a395b43
RealFlow <gashsh@yahoo.es>
2017-12-19 15:57:00 +0100

    work vip

--- app/scripts/work_vipor.js
+++ app/scripts/work_vipor.js
@@ -16,8 +16,8 @@ DG.Work.VIPOR = {
 					'<div class="row">'+
 					'<div class="col-lg-6 col-lg-offset-3">'+
 					'<h1>VIPOR</h1>'+
-					'<h4>Customer Relationship Management</h4>'+
-					'<h4>ROLE: TEAM LEAD DEVELOPER</h4>'+
+					'<h4>Customer Relationship Management (CRM)</h4>'+
+					'<h4>ROLE: DESKTOP & MOBILE LEAD DEVELOPER</h4>'+
 					'</div>'+
 					'</div>'+
 					'</div>'+
@@ -57,20 +57,26 @@ DG.Work.VIPOR = {
 					'<div class="col-lg-8 col-lg-offset-2">'+
 					'<h3 class="text-highlighted">ABOUT THE PROJECT</h3>'+
 					'<p align="justify">'+
-					'This is Vipor.'+
+					'Vipor CRM tools has been developed to help people make business relationships more meaningful and effective. Its aim is to provide business people with an affordable, easy-to-use anytime, anywhere solution that allows them to deal with people effectively, elevating the value of their relationship with each of them.'+
 					'</p>'+
 					'<p align="justify">'+
-					'TO BE WRITTEN'+
+					'This software provides everything needed to effectively manage contacts and build relationship simply from any iOS or macOS device, making very simple the task to track what you did with your clients, organize what you need to do for them, and get things done – so you can build better business relationships and be even more successful.'+
+					'</p>'+
+					'<p align="justify">'+
+					'The development has included'+
 					'</p>'+
 					'<br>'+
 					'<p align="justify">'+
-					'<span class="glyphicon glyphicon-ok" aria-hidden="true"></span><span class="text-highlighted"> TO BE WRITTEN:</span> Description.'+
+					'<span class="glyphicon glyphicon-ok" aria-hidden="true"></span><span class="text-highlighted"> Native macOS Application:</span> For all available macOS devices'+
 					'</p>'+
 					'<p align="justify">'+
-					'<span class="glyphicon glyphicon-ok" aria-hidden="true"></span><span class="text-highlighted"> TO BE WRITTEN:</span>  Description.'+
+					'<span class="glyphicon glyphicon-ok" aria-hidden="true"></span><span class="text-highlighted"> Native iOS Application:</span> For all available iPhone and iPad devices.'+
 					'</p>'+
 					'<p align="justify">'+
-					'<span class="glyphicon glyphicon-ok" aria-hidden="true"></span><span class="text-highlighted"> TO BE WRITTEN:</span>  Description.'+
+					'<span class="glyphicon glyphicon-ok" aria-hidden="true"></span><span class="text-highlighted"> Native iWatch Application:</span> For all available iWatch devices.'+
+					'</p>'+
+					'<p align="justify">'+
+					'<span class="glyphicon glyphicon-ok" aria-hidden="true"></span><span class="text-highlighted"> Backend:</span> Cloud-based Node.js server and Postgre database. REST interface written in TypeScript'+
 					'</p>'+
 					'</div>'+
 					'</div>';
@@ -87,127 +93,127 @@ DG.Work.VIPOR = {
 					'<div class="col-lg-10 col-lg-offset-1">'+
 					'<img class="centered platform" width="60px" src="images/portfolio_misc/gallery.png">'+
 					'<h4 class="text-highlighted">GALLERY</h4>'+
-					'<img class="img-responsive img-thumbnail" src="images/portfolio_content/work_vipor_1.png">'+
+					'<img class="img-responsive img-thumbnail" src="images/portfolio_content/work_vipor_4.png">'+
 					'<br>'+
 					''+
 					'<div id="gallery-title-1" style="display:none">'+
 					'<div class="custom-html">'+
-					'<h4 style="color: white">TO BE WRITTEN 1</h4>'+
+					'<h4 style="color: white">Login Views</h4>'+
 					'</div>'+
 					'</div>'+
 					'<div id="gallery-title-2" style="display:none">'+
 					'<div class="custom-html">'+
-					'<h4 style="color: white">TO BE WRITTEN 2</h4>'+
+					'<h4 style="color: white">Dashboards Views</h4>'+
 					'</div>'+
 					'</div>'+
 					'<div id="gallery-title-3" style="display:none">'+
 					'<div class="custom-html">'+
-					'<h4 style="color: white">TO BE WRITTEN 3</h4>'+
+					'<h4 style="color: white">Activity Edition Views</h4>'+
 					'</div>'+
 					'</div>'+
 					'<div id="gallery-title-4" style="display:none">'+
 					'<div class="custom-html">'+
-					'<h4 style="color: white">TO BE WRITTEN 4</h4>'+
+					'<h4 style="color: white">Activity in Dashboard Views</h4>'+
 					'</div>'+
 					'</div>'+
 					'<div id="gallery-title-5" style="display:none">'+
 					'<div class="custom-html">'+
-					'<h4 style="color: white">TO BE WRITTEN 5</h4>'+
+					'<h4 style="color: white">Orbits Menus</h4>'+
 					'</div>'+
 					'</div>'+
 					'<div id="gallery-title-6" style="display:none">'+
 					'<div class="custom-html">'+
-					'<h4 style="color: white">TO BE WRITTEN 6</h4>'+
+					'<h4 style="color: white">Orbit Edition Views</h4>'+
 					'</div>'+
 					'</div>'+
 					'<div id="gallery-title-7" style="display:none">'+
 					'<div class="custom-html">'+
-					'<h4 style="color: white">TO BE WRITTEN 7</h4>'+
+					'<h4 style="color: white">Orbit Detail Edition Views</h4>'+
 					'</div>'+
 					'</div>'+
 					'<div id="gallery-title-8" style="display:none">'+
 					'<div class="custom-html">'+
-					'<h4 style="color: white">TO BE WRITTEN 8</h4>'+
+					'<h4 style="color: white">Contact Activity Views</h4>'+
 					'</div>'+
 					'</div>'+
 					'<div id="gallery-title-9" style="display:none">'+
 					'<div class="custom-html">'+
-					'<h4 style="color: white">TO BE WRITTEN 9</h4>'+
+					'<h4 style="color: white">Empty Contact Activity Views</h4>'+
 					'</div>'+
 					'</div>'+
 					'<div id="gallery-title-10" style="display:none">'+
 					'<div class="custom-html">'+
-					'<h4 style="color: white">TO BE WRITTEN 10</h4>'+
+					'<h4 style="color: white">Contact Demographics Views</h4>'+
 					'</div>'+
 					'</div>'+
 					'<div id="gallery-title-11" style="display:none">'+
 					'<div class="custom-html">'+
-					'<h4 style="color: white">TO BE WRITTEN 11</h4>'+
+					'<h4 style="color: white">Empty Contact Demographics Views</h4>'+
 					'</div>'+
 					'</div>'+
 					'<div id="gallery-title-12" style="display:none">'+
 					'<div class="custom-html">'+
-					'<h4 style="color: white">TO BE WRITTEN 12</h4>'+
+					'<h4 style="color: white">Multi Contact Selection Views</h4>'+
 					'</div>'+
 					'</div>'+
 					'<div id="gallery-title-13" style="display:none">'+
 					'<div class="custom-html">'+
-					'<h4 style="color: white">TO BE WRITTEN 13/h4>'+
+					'<h4 style="color: white">Calendar Month Views</h4>'+
 					'</div>'+
 					'</div>'+
 					'<div id="gallery-title-14" style="display:none">'+
 					'<div class="custom-html">'+
-					'<h4 style="color: white">TO BE WRITTEN 14</h4>'+
+					'<h4 style="color: white">Calendar Week Views</h4>'+
 					'</div>'+
 					'</div>'+
 					'<div id="gallery-title-15" style="display:none">'+
 					'<div class="custom-html">'+
-					'<h4 style="color: white">TO BE WRITTEN 15</h4>'+
+					'<h4 style="color: white">Calendar List Views</h4>'+
 					'</div>'+
 					'</div>'+
 					'<div id="gallery-title-16" style="display:none">'+
 					'<div class="custom-html">'+
-					'<h4 style="color: white">TO BE WRITTEN 16</h4>'+
+					'<h4 style="color: white">Calendar Context Search Views</h4>'+
 					'</div>'+
 					'</div>'+
 					'<div id="gallery-title-17" style="display:none">'+
 					'<div class="custom-html">'+
-					'<h4 style="color: white">TO BE WRITTEN 17</h4>'+
+					'<h4 style="color: white">Contact Edition Views</h4>'+
 					'</div>'+
 					'</div>'+
 					'<div id="gallery-title-18" style="display:none">'+
 					'<div class="custom-html">'+
-					'<h4 style="color: white">TO BE WRITTEN 18</h4>'+
+					'<h4 style="color: white">Log Edition Views</h4>'+
 					'</div>'+
 					'</div>'+
 					'<div id="gallery-title-19" style="display:none">'+
 					'<div class="custom-html">'+
-					'<h4 style="color: white">TO BE WRITTEN 19</h4>'+
+					'<h4 style="color: white">Settings Views</h4>'+
 					'</div>'+
 					'</div>'+
 					'<div id="gallery-title-20" style="display:none">'+
 					'<div class="custom-html">'+
-					'<h4 style="color: white">TO BE WRITTEN 20</h4>'+
+					'<h4 style="color: white">Menu & Keyboard shortcuts Views</h4>'+
 					'</div>'+
 					'</div>'+
 					'<div id="gallery-title-21" style="display:none">'+
 					'<div class="custom-html">'+
-					'<h4 style="color: white">TO BE WRITTEN 21</h4>'+
+					'<h4 style="color: white">macOS Export All View</h4>'+
 					'</div>'+
 					'</div>'+
 					'<div id="gallery-title-22" style="display:none">'+
 					'<div class="custom-html">'+
-					'<h4 style="color: white">TO BE WRITTEN 22</h4>'+
+					'<h4 style="color: white">macOS Export All Result View</h4>'+
 					'</div>'+
 					'</div>'+
 					'<div id="gallery-title-23" style="display:none">'+
 					'<div class="custom-html">'+
-					'<h4 style="color: white">TO BE WRITTEN 23</h4>'+
+					'<h4 style="color: white">macOS Filtered Export View</h4>'+
 					'</div>'+
 					'</div>'+
 					'<div id="gallery-title-24" style="display:none">'+
 					'<div class="custom-html">'+
-					'<h4 style="color: white">TO BE WRITTEN 24</h4>'+
+					'<h4 style="color: white">macOS Filtered Export View</h4>'+
 					'</div>'+
 					'</div>'+
 					'</div>'+
